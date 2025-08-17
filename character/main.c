3ef14ea87fb98d42f94267b1b4bedd14f99220dc
sanaa <Sanaakumar07@gmail.com>
2025-08-17 18:57:44 +0530

    Initial Commit

---
+++ character/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  character
+//
+//  Created by Sanaa Kumar on 17/08/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
